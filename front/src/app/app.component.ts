4046b782effecd79c7a073667fb67d0111b8deb6
EmmanuelSantiz <emmanuel.07.01@hotmail.com>
2025-05-10 17:21:44 -0600

    add gitignore

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'e-Commerc Gapsi';
+  title = 'e-Commerce Gapsi';
   pageTitle = 'Dashboard';
 
   constructor(private router: Router) {
